a35f66f6eab5e12ad144a226adb4d3b4b69aebb8
Eric Luong <eluong330@gmail.com>
2017-02-27 21:20:52 -0800

    closed Indexer

--- src/Engine.java
+++ src/Engine.java
@@ -10,18 +10,25 @@ public class Engine
 		{
 			Indexer indexer = new Indexer("index");
 
+			System.out.println(indexer.getIndexer().numDocs());
+
 			// mapping from file directory -> url
 			HashMap<String, String> pages = FileOpener.getDirectories();
 
 			for (HashMap.Entry<String, String> kv : pages.entrySet())
 			{
-				System.out.println(kv.getKey());
+				//System.out.println(kv.getKey());
 				//System.out.println(Parser.parseHTML(kv.getKey()));
 
 				Document doc = new Document();
 
 				indexer.getIndexer().addDocument(doc);
 			}
+
+			System.out.println(indexer.getIndexer().numDocs());
+
+
+			indexer.getIndexer().close();
 		}
 		catch (Exception e)
 		{
