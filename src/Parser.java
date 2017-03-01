0ea50e67f7f2609f64c66874d0a05766a32fd520
Eric Luong <eluong330@gmail.com>
2017-02-28 17:16:47 -0800

    r u happy now dan

--- src/Parser.java
+++ src/Parser.java
@@ -2,6 +2,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.safety.Whitelist;
+import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,7 +10,8 @@ import java.util.HashMap;
 
 public class Parser
 {
-    private static String[] REQUIRED_TAGS = {"h1", "h2", "h3", "strong", "b", "body", "em"};
+    public static String[] REQUIRED_TAGS = {"h1", "h2", "h3", "strong", "b", "body", "em"};
+
     public static String parseHTML(String file_path) throws IOException
     {
         File html = new File(file_path);
@@ -27,10 +29,23 @@ public class Parser
 
         HashMap<String, String> content = new HashMap<>();
 
+        // TODO: Tokenize titles into own fields, or combine all tags into one field?
+
         for (String tag : REQUIRED_TAGS)
         {
+            System.out.println("^^^^^^vvvvvv\n" + tag + ":\n");
+
+            Elements contents = doc.select(tag);
             System.out.println(doc.select(tag));
+
             System.out.println("---");
+
+            for (Element e : contents)
+            {
+                System.out.println(e.ownText());
+            }
+
+            System.out.println("-----------");
         }
 
         return "";
