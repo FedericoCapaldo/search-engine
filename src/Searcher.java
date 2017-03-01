ae1cd9621c149b44526ac5b082c9703ac46c4576
Eric Luong <eluong330@gmail.com>
2017-02-28 23:50:52 -0800

    added weights for search DONT LOOK AT THIS

--- src/Searcher.java
+++ src/Searcher.java
@@ -9,11 +9,27 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Searcher
 {
+	private static Map<String, Float> FIELD_WEIGHTS;
+	static
+	{
+		FIELD_WEIGHTS = new HashMap<>();
+		FIELD_WEIGHTS.put("title", 100f);
+		FIELD_WEIGHTS.put("h1", 75f);
+		FIELD_WEIGHTS.put("h2", 60f);
+		FIELD_WEIGHTS.put("h3", 45f);
+		FIELD_WEIGHTS.put("strong", 30f);
+		FIELD_WEIGHTS.put("em", 25f);
+		FIELD_WEIGHTS.put("b", 25f);
+	}
+
 	private IndexSearcher indexSearcher;
 	private MultiFieldQueryParser queryParser;
+
 
 	public Searcher(IndexWriter indexWriter) throws IOException
 	{
